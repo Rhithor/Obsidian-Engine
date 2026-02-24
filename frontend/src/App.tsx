9496da2df0485dbb920795615d1390e6dae7a4bc
Rhithor <reynesh2005@gmail.com>
2026-02-24 22:43:36 +0530

    Added boilerplate code for the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+  <div>
+    <h1 className = "text-4xl font-bold text-blue-600 p-8 text-center">
+      Obsidian Engine
+    </h1>
+  </div>
+  );
+}
+
+export default App
